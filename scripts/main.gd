d33eb19c4aabfc33b0d1b52a16b347aab53994ef
Matureapple3258 <daniel13042008@gmail.com>
2026-06-03 06:37:20 +0100

    feat: player movement initial

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,14 @@
 extends Node2D
-
-var RegularEnemy: PackedScene = load("res://components/regular_enemy.tscn")
-@export var min_y: float = -100.0
-@export var max_y: float = 100.0
-@export var spawn_x: float = 0.0
-
-func _process(delta: float) -> void:
-	spawn_random_y()
-
-func spawn_random_y():
-	var y = randf_range(min_y, max_y)
-	var instance = RegularEnemy.instantiate()
-	instance.position = Vector2(spawn_x, y)
-	add_child(instance)
+#
+#var RegularEnemy: PackedScene = load("res://components/regular_enemy.tscn")
+#@export var min_y: float = -200.0
+#@export var max_y: float = 100.0
+#
+#func _process(delta: float) -> void:
+	#spawn_random_y()
+#
+#func spawn_random_y():
+	#var y = randf_range(min_y, max_y)
+	#var instance = RegularEnemy.instantiate()
+	#instance.position = Vector2(-100, y)
+	#add_child(instance)
