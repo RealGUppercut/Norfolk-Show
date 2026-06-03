6a2ea8d8221f0ec7d56ff130b76b96af641f00f3
Matureapple3258 <daniel13042008@gmail.com>
2026-06-03 13:46:21 +0100

    added better movement for the player and random enemy spawns with different sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,30 @@
 extends CharacterBody2D
 
-@export var speed = 200
-
+@export var max_speed = 300
+@export var default_speed = 100
+@export var speed = 100
+@export var time = 0
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
+	if velocity[0] == 0 and velocity[1] == 0:
+		$MovementTime.stop()
+		if $ResetMoveSpeed.is_stopped():
+			$ResetMoveSpeed.start()
+	else:
+		$MovementTime.start()
+		$ResetMoveSpeed.stop()
 	get_input()
 	move_and_slide()
+
+
+func _on_movement_time_timeout() -> void:
+	if speed >= max_speed:
+		return
+	else:
+		speed += 0.1
+
+func _on_reset_move_speed_timeout() -> void:
+	speed = default_speed
