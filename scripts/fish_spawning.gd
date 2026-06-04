3b8d3d689b7bfe8fd9b9df1bd7792fa9f4f204d3
Matureapple3258 <daniel13042008@gmail.com>
2026-06-04 14:01:36 +0100

    collisions work

--- scripts/fish_spawning.gd
+++ scripts/fish_spawning.gd
@@ -2,7 +2,7 @@ extends Timer
 
 var textures = ["res://sprites/entities/fish_1.png", ]
 
-var RegularEnemy: PackedScene = load("res://components/regular_enemy.tscn")
+var RegularFish: PackedScene = load("res://components/regular_fish.tscn")
 
 func _on_timeout() -> void:
 	spawn_random_y()
@@ -10,8 +10,8 @@ func _on_timeout() -> void:
 func spawn_random_y():
 	var y = randf_range(-100.0, 100.0)
 	var randNumber = randi_range(1, 4)
-	var instance = RegularEnemy.instantiate()
-	instance.position = Vector2(-500, y)
+	var instance = RegularFish.instantiate()
+	instance.position = Vector2(-250, y)
 	var sprite = instance.get_node("Mob/Sprite2D")
 	sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
 	get_parent().add_child(instance)
