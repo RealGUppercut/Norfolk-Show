22de64df5a83251caf8cb7656b355670b6c10d05
Eyi-tuoyo <eyituoyo117@gmail.com>
2026-06-08 16:23:59 +0100

    Resolve merge conflict

--- scripts/fish_spawning.gd
+++ scripts/fish_spawning.gd
@@ -18,44 +18,51 @@ var heart_chance = 5
 var net_chance = 8
 
 func _on_timeout() -> void:
-	spawn_random_y(0)
-	if DifficultyScale > 2:
+	if DifficultyScale >= 1 and DifficultyScale < 2:
+		spawn_random_y(1)
+	elif DifficultyScale >= 2 and DifficultyScale < 3:
 		spawn_random_y(2)
-	if DifficultyScale > 3:
-		spawn_random_y(-2)
+	elif DifficultyScale >= 3:
+		spawn_random_y(3)
 
-func spawn_random_y(y_add):
-	var instance = null
-	var IsEnemy = randi_range(0, 2)
+func spawn_random_y(AmountToSpawn):
+	var y_offset := 0
+	var y := randf_range(-100.0, 100.0)
 
-	if IsEnemy == 2:
-		var randNumber = randi_range(1, 4)
-		var StrongEnemy = randi_range(0, 3)
+	for i in range(AmountToSpawn):
+		var instance = null
+		var IsEnemy = randi_range(0, 2)
 
-		if StrongEnemy == 3:
-			var Enemy = EnemyList.pick_random()
-			instance = Enemy.instantiate()
+		if IsEnemy == 2:
+			var randNumber = randi_range(1, 4)
+			var StrongEnemy = randi_range(0, 3)
+			
+			if StrongEnemy == 3:
+				instance = EnemyList.pick_random().instantiate()
+			else:
+				instance = RegularEnemy.instantiate()
+
+			var sprite = instance.get_node("Mob/Sprite2D")
+			sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
 		else:
-			instance = RegularEnemy.instantiate()
+			var randNumber = randi_range(1, 4)
+			instance = RegularFish.instantiate()
 
-		var sprite = instance.get_node("Mob/Sprite2D")
-		sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
-	else:
-		var randNumber = randi_range(1, 4)
-		instance = RegularFish.instantiate()
+			var sprite = instance.get_node("Mob/Sprite2D")
+			sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
 
-		var sprite = instance.get_node("Mob/Sprite2D")
-		sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
+			# Assign powerup fish type
+			var fish_node = instance.get_node("Mob")
+			fish_node.fish_type = roll_fish_type()
 
-		# Assign powerup fish type
-		var fish_node = instance.get_node("Mob")
-		fish_node.fish_type = roll_fish_type()
+		var y_pos = y + y_offset
+		y_offset += randi_range(30, 100)
+		y_pos = wrapf(y_pos, -100, 100)
 
-	var y = randf_range(-100.0, 100.0)
-	instance.position = Vector2(-250, y + y_add)
-	instance.get_node("Mob").constant_force[0] = (FishSpeed * DifficultyScale)
+		instance.position = Vector2(-250, y_pos)
+		instance.get_node("Mob").constant_force.x = FishSpeed * DifficultyScale
 
-	get_parent().add_child(instance)
+		get_parent().add_child(instance)
 
 func roll_fish_type():
 	var roll = randi_range(1, 100)
