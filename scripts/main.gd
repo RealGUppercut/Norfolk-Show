653676f7be8e6c72f9565fa74bcc2cea1c36c3d6
marleylikesgit <marleycox.07@gmail.com>
2026-06-08 10:41:42 +0100

    added score and lives

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,23 @@
 extends Node2D
-#
-#var RegularEnemy: PackedScene = load("res://components/regular_enemy.tscn")
-#@export var min_y: float = -200.0
-#@export var max_y: float = 100.0
-#
-#func _process(delta: float) -> void:
-	#spawn_random_y()
-#
-#func spawn_random_y():
-	#var y = randf_range(min_y, max_y)
-	#var instance = RegularEnemy.instantiate()
-	#instance.position = Vector2(-100, y)
-	#add_child(instance)
-	
+var score = 0
+var lives = 5
+
+func _ready():
+	$ScoreLabel.text = "0"
+
+func add_score():
+	score += 3
+	$ScoreLabel.text = str(score)
+
+func lose_life():
+	print("lose life called, lives remaining: " + str(lives))
+	lives -= 1
+	get_node("Heart" + str(lives + 1)).visible = false
+	if lives <= 0:
+		game_over()
+
+func game_over():
+	get_tree().change_scene_to_file("res://components/main_menu.tscn")
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
