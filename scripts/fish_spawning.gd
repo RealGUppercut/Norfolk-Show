8c20f546dc12c8d048a0fa333c23e4310064e472
marleylikesgit <marleycox.07@gmail.com>
2026-06-09 16:24:02 +0100

    added seahorse enemy

--- scripts/fish_spawning.gd
+++ scripts/fish_spawning.gd
@@ -1,12 +1,12 @@
 extends Timer
 
 var textures = ["res://sprites/entities/fish_1.png"]
-
 var RegularFish: PackedScene = load("res://components/regular_fish.tscn")
 var RegularEnemy: PackedScene = load("res://components/regular_enemy.tscn")
+var Seahorse: PackedScene = load("res://components/seahorse.tscn")
 
 var EnemyList = [
-	RegularEnemy
+	load("res://components/regular_enemy.tscn"),  
 ]
 
 var SpawnTimeDefault = 2
@@ -16,6 +16,7 @@ var FishSpeed = 10
 # Powerup chances out of 100
 var heart_chance = 5
 var net_chance = 8
+var seahorse_chance = 10
 
 func _on_timeout() -> void:
 	if DifficultyScale >= 1 and DifficultyScale < 2:
@@ -28,49 +29,50 @@ func _on_timeout() -> void:
 func spawn_random_y(AmountToSpawn):
 	var y_offset := 0
 	var y := randf_range(-100.0, 100.0)
-
 	for i in range(AmountToSpawn):
 		var instance = null
 		var IsEnemy = randi_range(0, 2)
-
 		if IsEnemy == 2:
 			var randNumber = randi_range(1, 4)
 			var StrongEnemy = randi_range(0, 3)
-
 			if StrongEnemy == 3:
 				instance = EnemyList.pick_random().instantiate()
 			else:
 				instance = RegularEnemy.instantiate()
-
-			var sprite = instance.get_node("Mob/Sprite2D")
-			sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
+				var sprite = instance.get_node("Mob/Sprite2D")
+				sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
 		else:
-			var randNumber = randi_range(1, 4)
+			# checks to spawn seahorse
+			var fish_type = roll_fish_type()
+			if fish_type == 3:
+				var seahorse = Seahorse.instantiate()
+				var y_pos = y + y_offset
+				y_pos = wrapf(y_pos, -100, 100)
+				seahorse.position = Vector2(-250, y_pos)
+				get_parent().add_child(seahorse)
+				y_offset += randi_range(30, 100)
+				continue
 			instance = RegularFish.instantiate()
-
+			var randNumber = randi_range(1, 4)
 			var sprite = instance.get_node("Mob/Sprite2D")
 			sprite.texture = load("res://sprites/entities/fish_%d.png" % randNumber)
-
-			# Assign powerup fish type
 			var fish_node = instance.get_node("Mob")
-			fish_node.fish_type = roll_fish_type()
-
+			fish_node.fish_type = fish_type
 		var y_pos = y + y_offset
 		y_offset += randi_range(30, 100)
 		y_pos = wrapf(y_pos, -100, 100)
-
 		instance.position = Vector2(-250, y_pos)
 		instance.get_node("Mob").constant_force.x = FishSpeed * DifficultyScale
-
 		get_parent().add_child(instance)
 
 func roll_fish_type():
 	var roll = randi_range(1, 100)
-
 	if roll <= heart_chance:
 		return 2  # HEART
 	elif roll <= heart_chance + net_chance:
 		return 1  # NET
+	elif roll <= heart_chance + net_chance + seahorse_chance:
+		return 3  # SEAHORSE
 	else:
 		return 0  # REGULAR
 
---
+++ scripts/seahorse.gd
@@ -0,0 +1,33 @@
+extends RigidBody2D
+@export var speed = 80
+var is_mini = false
+
+func _ready():
+	gravity_scale = 0
+	linear_velocity = Vector2(speed, 0)
+	if is_mini:
+		scale = Vector2(0.5, 0.5)
+
+func on_caught():
+	if not is_mini:
+		GameEvent.score_added.emit(10)
+		spawn_mini_seahorses()
+	else:
+		GameEvent.score_added.emit(5)
+		queue_free()
+
+func spawn_mini_seahorses():
+	for i in range(5):
+		var mini = load("res://components/seahorse.tscn").instantiate()
+		mini.is_mini = true
+		mini.position = position + Vector2(randf_range(-50, 50), randf_range(-50, 50))
+		mini.linear_velocity = Vector2(randf_range(60, 120), randf_range(-30, 30))
+		get_parent().add_child(mini)
+	queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		on_caught()
