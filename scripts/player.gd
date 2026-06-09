c6fb1c19e3cbf49d64d9e61474f8c086d7d5b721
Matureapple3258 <daniel13042008@gmail.com>
2026-06-09 10:45:23 +0100

    added changes to the player speed and the difficulty scaling.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,8 @@
 extends CharacterBody2D
-<<<<<<< HEAD
 
 @export var max_speed = 300
-@export var default_speed = 100
-=======
-@export var max_speed = 350
 @export var default_speed = 150
->>>>>>> origin/main
-@export var speed = 100
+@export var speed = 150
 @export var time = 0
 
 var net_scale = Vector2(1, 1)
