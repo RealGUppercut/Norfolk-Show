95369eb2dffd8ccc546378895b20675679a1b178
Matureapple3258 <daniel13042008@gmail.com>
2026-06-18 14:58:51 +0100

    added compendium

--- mainmenu.gd
+++ mainmenu.gd
@@ -24,4 +24,4 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_button_compendium_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://components/compendium.tscn")
